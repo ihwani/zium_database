773fcc29380d92bc87911bad30d7750aea72ddd4
ihwani <33796218+ihwani@users.noreply.github.com>
2022-05-05 20:36:27 +0900

    .

---
+++ excel_to_json.py
@@ -0,0 +1,33 @@
+import openpyxl
+import json
+
+excel_file = 'C:\github\python/zium_database.xlsx'
+json_file = 'C:\github\python/zium_database.json'
+
+wb = openpyxl.load_workbook(excel_file, read_only=True, data_only=True)
+
+sheet = wb.worksheets[0]
+
+key_list = []
+for col_num in range(1, sheet.max_column + 1):
+    key_list.append(sheet.cell(row=1, column=col_num).value)
+
+data_dict = []
+for row_num in range(2, sheet.max_row + 1):
+    tmp_dict = {}
+    tag = ""
+    for col_num in range(1, sheet.max_column + 1):
+        val = sheet.cell(row=row_num, column=col_num).value
+        if col_num < 11:
+            tmp_dict[key_list[col_num - 1]] = val
+        else:
+            tag = sheet.cell(row=row_num, column=col_num).value
+    tag = tag.split(',')
+    tmp_dict[key_list[10]] = tag
+    print(tmp_dict)
+    data_dict.append(tmp_dict)
+
+wb.close()
+
+with open(json_file, 'w', encoding='utf-8') as fp:
+    json.dump(data_dict, fp, indent=4, ensure_ascii=False)
