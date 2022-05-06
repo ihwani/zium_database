a43605a4b939c62d6b6ed20bd6866ab9ca4d515d
ihwani <33796218+ihwani@users.noreply.github.com>
2022-05-06 16:36:10 +0900

    .

--- excel_to_json.py
+++ excel_to_json.py
@@ -1,33 +1,406 @@
-import openpyxl
-import json
-
-excel_file = 'C:\github\zium_database/zium_database.xlsx'
-json_file = 'C:\github\zium_database/zium_database.json'
-
-wb = openpyxl.load_workbook(excel_file, read_only=True, data_only=True)
-
-sheet = wb.worksheets[0]
-
-key_list = []
-for col_num in range(1, sheet.max_column + 1):
-    key_list.append(sheet.cell(row=1, column=col_num).value)
-
-data_dict = []
-for row_num in range(2, sheet.max_row + 1):
-    tmp_dict = {}
-    tag = ""
-    for col_num in range(1, sheet.max_column + 1):
-        val = sheet.cell(row=row_num, column=col_num).value
-        if col_num < 11:
-            tmp_dict[key_list[col_num - 1]] = val
-        else:
-            tag = sheet.cell(row=row_num, column=col_num).value
-    tag = tag.split(',')
-    tmp_dict[key_list[10]] = tag
-    print(tmp_dict)
-    data_dict.append(tmp_dict)
-
-wb.close()
-
-with open(json_file, 'w', encoding='utf-8') as fp:
-    json.dump(data_dict, fp, indent=4, ensure_ascii=False)
+[
+    {
+        "id": "m_00001_pj_00001",
+        "office_id": "m_00001",
+        "design_office": "미소로건축사사무소",
+        "homepage_link": "http://misoroarch.com",
+        "ic_link": "http://misoroarch.com/images/main/mt_title.png",
+        "project_id": "pj_00001",
+        "project_name": "갤러리84(75)",
+        "project_link": "http://misoroarch.com/HyAdmin/view.php?&bbs_id=bo02&page=&doc_num=36",
+        "image_link": "http://misoroarch.com/HyAdmin/naverEditor/upload/1642754249.png",
+        "location": "인천시 영종도 운북동",
+        "tag": [
+            "상가주택",
+            " 근린생활시설",
+            " 다가구주택",
+            " 주택",
+            " 상가"
+        ]
+    },
+    {
+        "id": "m_00001_pj_00002",
+        "office_id": "m_00001",
+        "design_office": "미소로건축사사무소",
+        "homepage_link": "http://misoroarch.com",
+        "ic_link": "http://misoroarch.com/images/main/mt_title.png",
+        "project_id": "pj_00002",
+        "project_name": "대치동 근린생활시설",
+        "project_link": "http://misoroarch.com/HyAdmin/view.php?&bbs_id=bo02&page=&doc_num=20",
+        "image_link": "http://misoroarch.com/HyAdmin/naverEditor/upload/1619583051.jpg",
+        "location": "서울시 강남구 대치동",
+        "tag": [
+            "근린생활시설",
+            " 상가"
+        ]
+    },
+    {
+        "id": "m_00001_pj_00003",
+        "office_id": "m_00001",
+        "design_office": "미소로건축사사무소",
+        "homepage_link": "http://misoroarch.com",
+        "ic_link": "http://misoroarch.com/images/main/mt_title.png",
+        "project_id": "pj_00003",
+        "project_name": "성남시 고등동 근생 및 다세대주택",
+        "project_link": "http://misoroarch.com/HyAdmin/view.php?&bbs_id=bo02&page=&doc_num=23",
+        "image_link": "http://misoroarch.com/HyAdmin/naverEditor/upload/1620023226.jpg",
+        "location": "경기도 성남시 수정구 고등동",
+        "tag": [
+            "상가주택",
+            " 근린생활시설",
+            " 다가구주택",
+            " 주택",
+            " 상가"
+        ]
+    },
+    {
+        "id": "m_00001_pj_00004",
+        "office_id": "m_00001",
+        "design_office": "미소로건축사사무소",
+        "homepage_link": "http://misoroarch.com",
+        "ic_link": "http://misoroarch.com/images/main/mt_title.png",
+        "project_id": "pj_00004",
+        "project_name": "구미 직장 어린이집",
+        "project_link": "http://misoroarch.com/HyAdmin/view.php?&bbs_id=bo02&page=&doc_num=31",
+        "image_link": "http://misoroarch.com/HyAdmin/naverEditor/upload/1624342016.jpg",
+        "location": "경상북도 구미시 구포동",
+        "tag": [
+            "노유자시설"
+        ]
+    },
+    {
+        "id": "s_00001_pj_00001",
+        "office_id": "s_00001",
+        "design_office": "소수 건축사사무소",
+        "homepage_link": "http://sosu2357.com/",
+        "ic_link": "http://images.squarespace-cdn.com/content/v1/5756c135356fb02fbe7ced1d/1468466155608-AQICUKO1PT078R8MGRBO/sosu+ci-01+copy.png",
+        "project_id": "pj_00001",
+        "project_name": "시선재(示線齋)",
+        "project_link": "http://sosu2357.com/projects/seasunjae",
+        "image_link": "https://images.squarespace-cdn.com/content/v1/5756c135356fb02fbe7ced1d/1487048537891-P5OM894V6SS0G4VO8UIY/%EC%8B%9C%EC%84%A0%EC%9E%AC+%EC%A0%95%EB%A9%B4%EC%82%AC%EC%A7%84%28%EC%88%98%EC%A0%95%ED%9B%84%29.jpg",
+        "location": "제주도 서귀포시 대포동",
+        "tag": [
+            "단독주택",
+            " 주택"
+        ]
+    },
+    {
+        "id": "s_00001_pj_00002",
+        "office_id": "s_00001",
+        "design_office": "소수 건축사사무소",
+        "homepage_link": "http://sosu2357.com/",
+        "ic_link": "http://images.squarespace-cdn.com/content/v1/5756c135356fb02fbe7ced1d/1468466155608-AQICUKO1PT078R8MGRBO/sosu+ci-01+copy.png",
+        "project_id": "pj_00002",
+        "project_name": "담류헌(談流軒)",
+        "project_link": "http://sosu2357.com/projects/8456",
+        "image_link": "https://images.squarespace-cdn.com/content/v1/5756c135356fb02fbe7ced1d/1467600129226-HT31Q3XJIV68Q7RSXKS7/KakaoTalk_20160630_201846235.jpg",
+        "location": "경기도 파주시 산남동",
+        "tag": [
+            "단독주택",
+            " 주택"
+        ]
+    },
+    {
+        "id": "s_00001_pj_00003",
+        "office_id": "s_00001",
+        "design_office": "소수 건축사사무소",
+        "homepage_link": "http://sosu2357.com/",
+        "ic_link": "http://images.squarespace-cdn.com/content/v1/5756c135356fb02fbe7ced1d/1468466155608-AQICUKO1PT078R8MGRBO/sosu+ci-01+copy.png",
+        "project_id": "pj_00003",
+        "project_name": "동심원(同心院)",
+        "project_link": "http://sosu2357.com/projects/000",
+        "image_link": "https://images.squarespace-cdn.com/content/v1/5756c135356fb02fbe7ced1d/1503938393715-BVIIXNSSO78REBDM8SGO/PRIZE-2.jpg",
+        "location": "서울시 양천구 목동",
+        "tag": [
+            "상가주택",
+            " 주택",
+            " 상가"
+        ]
+    },
+    {
+        "id": "s_00001_pj_00004",
+        "office_id": "s_00001",
+        "design_office": "소수 건축사사무소",
+        "homepage_link": "http://sosu2357.com/",
+        "ic_link": "http://images.squarespace-cdn.com/content/v1/5756c135356fb02fbe7ced1d/1468466155608-AQICUKO1PT078R8MGRBO/sosu+ci-01+copy.png",
+        "project_id": "pj_00004",
+        "project_name": "지품 팡팡 놀이터",
+        "project_link": "http://sosu2357.com/projects/pangpang",
+        "image_link": "https://images.squarespace-cdn.com/content/v1/5756c135356fb02fbe7ced1d/1500613997236-CBR414WDKLEI849SC2IU/DSC_9094-편집.jpg",
+        "location": "경상북도 영덕군 지품면",
+        "tag": [
+            "지역복지시설"
+        ]
+    },
+    {
+        "id": "s_00001_pj_00005",
+        "office_id": "s_00001",
+        "design_office": "소수 건축사사무소",
+        "homepage_link": "http://sosu2357.com/",
+        "ic_link": "http://images.squarespace-cdn.com/content/v1/5756c135356fb02fbe7ced1d/1468466155608-AQICUKO1PT078R8MGRBO/sosu+ci-01+copy.png",
+        "project_id": "pj_00005",
+        "project_name": "정중헌(正中軒)",
+        "project_link": "http://sosu2357.com/projects/sejong",
+        "image_link": "https://images.squarespace-cdn.com/content/v1/5756c135356fb02fbe7ced1d/1525185810809-W2O16CWI0UWG3306PK9G/2.jpg",
+        "location": ".",
+        "tag": [
+            "단독주택",
+            " 주택"
+        ]
+    },
+    {
+        "id": "s_00001_pj_00006",
+        "office_id": "s_00001",
+        "design_office": "소수 건축사사무소",
+        "homepage_link": "http://sosu2357.com/",
+        "ic_link": "http://images.squarespace-cdn.com/content/v1/5756c135356fb02fbe7ced1d/1468466155608-AQICUKO1PT078R8MGRBO/sosu+ci-01+copy.png",
+        "project_id": "pj_00006",
+        "project_name": "신선길(Shinsun-gil)",
+        "project_link": "http://sosu2357.com/projects/250",
+        "image_link": "https://images.squarespace-cdn.com/content/v1/5756c135356fb02fbe7ced1d/1530938387870-UN9DFCKF5A24O1RB2Y8T/4상부1.jpg",
+        "location": ".",
+        "tag": [
+            "전망대"
+        ]
+    },
+    {
+        "id": "s_00001_pj_00007",
+        "office_id": "s_00001",
+        "design_office": "소수 건축사사무소",
+        "homepage_link": "http://sosu2357.com/",
+        "ic_link": "http://images.squarespace-cdn.com/content/v1/5756c135356fb02fbe7ced1d/1468466155608-AQICUKO1PT078R8MGRBO/sosu+ci-01+copy.png",
+        "project_id": "pj_00007",
+        "project_name": "닮담집(DamDamZip)",
+        "project_link": "http://sosu2357.com/projects/ssang",
+        "image_link": "https://images.squarespace-cdn.com/content/v1/5756c135356fb02fbe7ced1d/1537181229797-2ZC550DRPXQ4BM1PPNE9/thumnai-2.jpg",
+        "location": "서울시 도봉구 쌍문동",
+        "tag": [
+            "다가구주택",
+            " 주택"
+        ]
+    },
+    {
+        "id": "s_00001_pj_00008",
+        "office_id": "s_00001",
+        "design_office": "소수 건축사사무소",
+        "homepage_link": "http://sosu2357.com/",
+        "ic_link": "http://images.squarespace-cdn.com/content/v1/5756c135356fb02fbe7ced1d/1468466155608-AQICUKO1PT078R8MGRBO/sosu+ci-01+copy.png",
+        "project_id": "pj_00008",
+        "project_name": "일삶빌딩(3/1 bldg.)",
+        "project_link": "http://sosu2357.com/projects/315-37",
+        "image_link": "https://images.squarespace-cdn.com/content/v1/5756c135356fb02fbe7ced1d/1542679029822-LBGXA8V0KJ3JSWXPVISQ/thumnail.jpg",
+        "location": "서울시 성동구 성수동2가",
+        "tag": [
+            "상가주택",
+            " 근린생활시설",
+            " 다가구주택",
+            " 주택",
+            " 상가"
+        ]
+    },
+    {
+        "id": "s_00001_pj_00009",
+        "office_id": "s_00001",
+        "design_office": "소수 건축사사무소",
+        "homepage_link": "http://sosu2357.com/",
+        "ic_link": "http://images.squarespace-cdn.com/content/v1/5756c135356fb02fbe7ced1d/1468466155608-AQICUKO1PT078R8MGRBO/sosu+ci-01+copy.png",
+        "project_id": "pj_00009",
+        "project_name": "송학리의 생각(生閣)",
+        "project_link": "http://sosu2357.com/projects//-",
+        "image_link": "https://images.squarespace-cdn.com/content/v1/5756c135356fb02fbe7ced1d/1573812648846-VYJEJF1JHLMIKXQN84YD/thum.jpg",
+        "location": "경기도 양평군 강상면 송학리",
+        "tag": [
+            "단독주택",
+            " 주택"
+        ]
+    },
+    {
+        "id": "t_00001_pj_00001",
+        "office_id": "t_00001",
+        "design_office": "투닷 건축사사무소",
+        "homepage_link": "https://todot.kr/",
+        "ic_link": "https://blogpfthumb-phinf.pstatic.net/MjAxOTEyMjVfMjA1/MDAxNTc3MjE4Nzc2NTIy.EkeacEGWYCB5ib35H9Nfckt-RwihhCo-qfEnGCfdxBYg.AsYOAQpr4BPrRwDMhO9cSokLM2FPjk3bA1kJs4G2ubAg.JPEG.ftw18/%25C5%25F5%25B4%25E5%2B%25BA%25B9%25BB%25E7.jpg",
+        "project_id": "pj_00001",
+        "project_name": "김포 근린생활시설",
+        "project_link": "https://todot.kr/29?category=821064",
+        "image_link": "https://i1.daumcdn.net/thumb/C230x180/?fname=https://blog.kakaocdn.net/dn/dSnR2J/btqzrXWI0SD/v0HNR4c6nVggsyWIgFggo1/img.jpg",
+        "location": "경기도 김포시 고촌읍 전호리",
+        "tag": [
+            "근린생활시설",
+            " 상가"
+        ]
+    },
+    {
+        "id": "t_00001_pj_00002",
+        "office_id": "t_00001",
+        "design_office": "투닷 건축사사무소",
+        "homepage_link": "https://todot.kr/",
+        "ic_link": "https://blogpfthumb-phinf.pstatic.net/MjAxOTEyMjVfMjA1/MDAxNTc3MjE4Nzc2NTIy.EkeacEGWYCB5ib35H9Nfckt-RwihhCo-qfEnGCfdxBYg.AsYOAQpr4BPrRwDMhO9cSokLM2FPjk3bA1kJs4G2ubAg.JPEG.ftw18/%25C5%25F5%25B4%25E5%2B%25BA%25B9%25BB%25E7.jpg",
+        "project_id": "pj_00002",
+        "project_name": "LS전선 동해사업장 본관동 증축",
+        "project_link": "https://todot.kr/87?category=821064",
+        "image_link": "https://i1.daumcdn.net/thumb/C230x180/?fname=https://blog.kakaocdn.net/dn/kNXVk/btrqQfriMDY/nliDnjkhioDRiNzRXmSXq0/img.jpg",
+        "location": "강원도 동해시 송정동",
+        "tag": [
+            "공장"
+        ]
+    },
+    {
+        "id": "t_00001_pj_00003",
+        "office_id": "t_00001",
+        "design_office": "투닷 건축사사무소",
+        "homepage_link": "https://todot.kr/",
+        "ic_link": "https://blogpfthumb-phinf.pstatic.net/MjAxOTEyMjVfMjA1/MDAxNTc3MjE4Nzc2NTIy.EkeacEGWYCB5ib35H9Nfckt-RwihhCo-qfEnGCfdxBYg.AsYOAQpr4BPrRwDMhO9cSokLM2FPjk3bA1kJs4G2ubAg.JPEG.ftw18/%25C5%25F5%25B4%25E5%2B%25BA%25B9%25BB%25E7.jpg",
+        "project_id": "pj_00003",
+        "project_name": "화성 단독주택 '봉구네",
+        "project_link": "https://todot.kr/50?category=821063",
+        "image_link": "https://i1.daumcdn.net/thumb/C230x180/?fname=https://blog.kakaocdn.net/dn/qLD2T/btqCM9AJ15n/xtqKY9KcHEsm5zy7Xn3GHk/img.png",
+        "location": "경기도 화성시 정남면 수면리",
+        "tag": [
+            "단독주택",
+            " 주택"
+        ]
+    },
+    {
+        "id": "t_00001_pj_00004",
+        "office_id": "t_00001",
+        "design_office": "투닷 건축사사무소",
+        "homepage_link": "https://todot.kr/",
+        "ic_link": "https://blogpfthumb-phinf.pstatic.net/MjAxOTEyMjVfMjA1/MDAxNTc3MjE4Nzc2NTIy.EkeacEGWYCB5ib35H9Nfckt-RwihhCo-qfEnGCfdxBYg.AsYOAQpr4BPrRwDMhO9cSokLM2FPjk3bA1kJs4G2ubAg.JPEG.ftw18/%25C5%25F5%25B4%25E5%2B%25BA%25B9%25BB%25E7.jpg",
+        "project_id": "pj_00004",
+        "project_name": "정자동 다가구주택 ‘솟은집’",
+        "project_link": "https://todot.kr/51?category=821063",
+        "image_link": "https://i1.daumcdn.net/thumb/C230x180/?fname=https://blog.kakaocdn.net/dn/bp2wQt/btqCM9Hnj4d/i3NKohoOvGld9uj8CixuUK/img.jpg",
+        "location": "경기도 수원시 장안구 정자동",
+        "tag": [
+            "상가주택",
+            " 근린생활시설",
+            " 상가"
+        ]
+    },
+    {
+        "id": "t_00001_pj_00005",
+        "office_id": "t_00001",
+        "design_office": "투닷 건축사사무소",
+        "homepage_link": "https://todot.kr/",
+        "ic_link": "https://blogpfthumb-phinf.pstatic.net/MjAxOTEyMjVfMjA1/MDAxNTc3MjE4Nzc2NTIy.EkeacEGWYCB5ib35H9Nfckt-RwihhCo-qfEnGCfdxBYg.AsYOAQpr4BPrRwDMhO9cSokLM2FPjk3bA1kJs4G2ubAg.JPEG.ftw18/%25C5%25F5%25B4%25E5%2B%25BA%25B9%25BB%25E7.jpg",
+        "project_id": "pj_00005",
+        "project_name": "용인 서천 상가주택",
+        "project_link": "https://todot.kr/36?category=821063",
+        "image_link": "https://i1.daumcdn.net/thumb/C230x180/?fname=https://blog.kakaocdn.net/dn/zINPa/btqzH1xsDq4/U8IL6Ybpuy7xY6mV1dLMtK/img.jpg",
+        "location": "경기도 용인시 기흥구 농서동",
+        "tag": [
+            "상가주택",
+            " 근린생활시설",
+            " 다가구주택",
+            " 주택",
+            " 상가"
+        ]
+    },
+    {
+        "id": "t_00001_pj_00006",
+        "office_id": "t_00001",
+        "design_office": "투닷 건축사사무소",
+        "homepage_link": "https://todot.kr/",
+        "ic_link": "https://blogpfthumb-phinf.pstatic.net/MjAxOTEyMjVfMjA1/MDAxNTc3MjE4Nzc2NTIy.EkeacEGWYCB5ib35H9Nfckt-RwihhCo-qfEnGCfdxBYg.AsYOAQpr4BPrRwDMhO9cSokLM2FPjk3bA1kJs4G2ubAg.JPEG.ftw18/%25C5%25F5%25B4%25E5%2B%25BA%25B9%25BB%25E7.jpg",
+        "project_id": "pj_00006",
+        "project_name": "용인 서천 삼남매집",
+        "project_link": "https://todot.kr/37?category=821063",
+        "image_link": "https://i1.daumcdn.net/thumb/C230x180/?fname=https://blog.kakaocdn.net/dn/l1qpu/btqzKdD8U7u/92hpRS47zebbuKGLFweOeK/img.jpg",
+        "location": "경기도 용인시 기흥구 농서동",
+        "tag": [
+            "상가주택",
+            " 근린생활시설",
+            " 다가구주택",
+            " 주택",
+            " 상가"
+        ]
+    },
+    {
+        "id": "t_00001_pj_00007",
+        "office_id": "t_00001",
+        "design_office": "투닷 건축사사무소",
+        "homepage_link": "https://todot.kr/",
+        "ic_link": "https://blogpfthumb-phinf.pstatic.net/MjAxOTEyMjVfMjA1/MDAxNTc3MjE4Nzc2NTIy.EkeacEGWYCB5ib35H9Nfckt-RwihhCo-qfEnGCfdxBYg.AsYOAQpr4BPrRwDMhO9cSokLM2FPjk3bA1kJs4G2ubAg.JPEG.ftw18/%25C5%25F5%25B4%25E5%2B%25BA%25B9%25BB%25E7.jpg",
+        "project_id": "pj_00007",
+        "project_name": "파주 단독주택",
+        "project_link": "https://todot.kr/11?category=821063",
+        "image_link": "https://i1.daumcdn.net/thumb/C230x180/?fname=https://blog.kakaocdn.net/dn/trMtk/btqzb9W8Dzq/cWHqEqJQtHhsOdIi0oHpK0/img.jpg",
+        "location": "경기도 용인시 기흥구 농서동",
+        "tag": [
+            "단독주택",
+            " 주택"
+        ]
+    },
+    {
+        "id": "t_00001_pj_00008",
+        "office_id": "t_00001",
+        "design_office": "투닷 건축사사무소",
+        "homepage_link": "https://todot.kr/",
+        "ic_link": "https://blogpfthumb-phinf.pstatic.net/MjAxOTEyMjVfMjA1/MDAxNTc3MjE4Nzc2NTIy.EkeacEGWYCB5ib35H9Nfckt-RwihhCo-qfEnGCfdxBYg.AsYOAQpr4BPrRwDMhO9cSokLM2FPjk3bA1kJs4G2ubAg.JPEG.ftw18/%25C5%25F5%25B4%25E5%2B%25BA%25B9%25BB%25E7.jpg",
+        "project_id": "pj_00008",
+        "project_name": "원주 상가주택 '자경채(自景彩)'",
+        "project_link": "https://todot.kr/20?category=821063",
+        "image_link": "https://i1.daumcdn.net/thumb/C230x180/?fname=https://blog.kakaocdn.net/dn/ceCwNk/btqzjhaM2N6/lI0lgyNVKIE6EJuovhKlf0/img.jpg",
+        "location": "경기도 용인시 기흥구 농서동",
+        "tag": [
+            "상가주택",
+            " 근린생활시설",
+            " 다가구주택",
+            " 주택",
+            " 상가"
+        ]
+    },
+    {
+        "id": "t_00001_pj_00009",
+        "office_id": "t_00001",
+        "design_office": "투닷 건축사사무소",
+        "homepage_link": "https://todot.kr/",
+        "ic_link": "https://blogpfthumb-phinf.pstatic.net/MjAxOTEyMjVfMjA1/MDAxNTc3MjE4Nzc2NTIy.EkeacEGWYCB5ib35H9Nfckt-RwihhCo-qfEnGCfdxBYg.AsYOAQpr4BPrRwDMhO9cSokLM2FPjk3bA1kJs4G2ubAg.JPEG.ftw18/%25C5%25F5%25B4%25E5%2B%25BA%25B9%25BB%25E7.jpg",
+        "project_id": "pj_00009",
+        "project_name": "군자동 다세대주택 '밭은집'",
+        "project_link": "https://todot.kr/12?category=821063",
+        "image_link": "https://i1.daumcdn.net/thumb/C230x180/?fname=https://blog.kakaocdn.net/dn/OjkHB/btqza9wZwcf/kLVYVcuRbAhyAzNXaECZ30/img.jpg",
+        "location": "경기도 용인시 기흥구 농서동",
+        "tag": [
+            "상가주택",
+            " 근린생활시설",
+            " 다세대주택",
+            " 주택",
+            " 상가"
+        ]
+    },
+    {
+        "id": "i_00001_pj_00001",
+        "office_id": "i_00001",
+        "design_office": "인우 건축사사무소",
+        "homepage_link": "http://inuarchi.com/",
+        "ic_link": "http://inuarchi.com/wp-content/uploads/2020/10/%EB%A1%9C%EA%B3%A0-e1603675402751.png",
+        "project_id": "pj_00001",
+        "project_name": "장성 단독주택",
+        "project_link": "http://inuarchi.com/?p=1868",
+        "image_link": "http://inuarchi.com/wp-content/uploads/2020/10/1-2-300x150.jpg",
+        "location": ".",
+        "tag": [
+            "단독주택",
+            " 주택"
+        ]
+    },
+    {
+        "id": "i_00001_pj_00002",
+        "office_id": "i_00001",
+        "design_office": "인우 건축사사무소",
+        "homepage_link": "http://inuarchi.com/",
+        "ic_link": "http://inuarchi.com/wp-content/uploads/2020/10/%EB%A1%9C%EA%B3%A0-e1603675402751.png",
+        "project_id": "pj_00002",
+        "project_name": "양림동 상가주택",
+        "project_link": "http://inuarchi.com/?p=1938",
+        "image_link": "http://inuarchi.com/wp-content/uploads/2020/10/DSC3724-300x150.jpg",
+        "location": ".",
+        "tag": [
+            "상가주택",
+            " 상가",
+            " 주택"
+        ]
+    }
+]
