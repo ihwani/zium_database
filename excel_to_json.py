0e17a0093afbcadd0574439cbf626504970df787
ihwani <33796218+ihwani@users.noreply.github.com>
2022-05-09 21:38:36 +0900

    .

--- excel_to_json.py
+++ excel_to_json.py
@@ -2,11 +2,11 @@ import json
 import openpyxl
 
 # Pc path
-excel_file = 'C:/github/zium_database/zium_database.xlsx'
-json_file = 'C:/github/zium_database/zium_database.json'
+# excel_file = 'C:/github/zium_database/zium_database.xlsx'
+# json_file = 'C:/github/zium_database/zium_database.json'
 # Mac path
-# excel_file = '/Users/ihwani/Documents/GitHub/zium_database/zium_database.xlsx'
-# json_file = '/Users/ihwani/Documents/GitHub/zium_database/zium_database.json'
+excel_file = '/Users/ihwani/Documents/GitHub/zium_database/zium_database.xlsx'
+json_file = '/Users/ihwani/Documents/GitHub/zium_database/zium_database.json'
 
 wb = openpyxl.load_workbook(excel_file, read_only=True, data_only=True)
 
@@ -20,14 +20,21 @@ data_dict = []
 for row_num in range(2, sheet.max_row + 1):
     tmp_dict = {}
     tag = ""
+    # boss = ""
     for col_num in range(1, sheet.max_column + 1):
         val = sheet.cell(row=row_num, column=col_num).value
-        if col_num < 11:
+        if col_num == 6:
+            boss = sheet.cell(row=row_num, column=col_num).value
+        elif col_num < sheet.max_column:
             tmp_dict[key_list[col_num - 1]] = val
         else:
             tag = sheet.cell(row=row_num, column=col_num).value
+    boss = boss.split(',')
+    boss = [line.strip() for line in boss]
     tag = tag.split(',')
-    tmp_dict[key_list[10]] = tag
+    tag = [line.strip() for line in tag]
+    tmp_dict[key_list[5]] = boss
+    tmp_dict[key_list[-1]] = tag
     data_dict.append(tmp_dict)
 
 wb.close()
