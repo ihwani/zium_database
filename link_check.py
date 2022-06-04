7d2530d7fccb677c72c63941d9edd5ffdc1da222
ihwani <33796218+ihwani@users.noreply.github.com>
2022-06-05 06:28:30 +0900

    .

--- link_check.py
+++ link_check.py
@@ -1,7 +1,5 @@
-from urllib import request
 import openpyxl
 import requests
-from tqdm import tqdm
 
 # Pc path
 file_path = 'C:/github/zium_database'
@@ -16,17 +14,26 @@ wb = openpyxl.load_workbook(excel_file, read_only=True, data_only=True)
 sheet = wb.worksheets[0]
 
 url_list = []
-for address_row_num in range(2, sheet.max_row + 1):
+for address_row_num in range(2, sheet.max_row):
     url_list.append(sheet.cell(
         row=address_row_num, column=12).value)
 
-
-print(len(url_list))
-
-wb.close()
+check_result_pass = []
+check_result_fail = []
 
 for i in range(len(url_list)):
     if requests.get(url_list[i]).status_code == 200:
-        print(str(i)+'pass')
+        result = str(i)+' - pass'
+        check_result_pass.append(result)
     else:
-        print(str(i)+'error')
+        result = str(i)+' - error'
+        check_result_fail.append(result)
+    print(i)
+
+if len(check_result_fail) == 0:
+    print('Sucess')
+else:
+    print(len(check_result_fail))
+    print(check_result_fail)
+
+wb.close
